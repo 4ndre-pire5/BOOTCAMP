b8dd9ea97d98f721d1417dc05ae943702a9a1a90
Andre Pires <andre.figueiredo@aluno.faculdadeimpacta.com.br>
2023-12-12 16:24:43 -0300

    ajustando pagina inicial

--- front-end/src/app/home/Div1_0/index.tsx
+++ front-end/src/app/home/Div1_0/index.tsx
@@ -4,12 +4,13 @@ import styles from '@/app/home/Div1_0/styles.module.css'
 export const Div1 = () => {
     return(
         <div className={styles.Div1}>
-            <Image
-                src="/Foto centroteste.png"
-                width={1800} 
-                height={507}
-                alt="Figura centroteste"
-            />
+            <div className={styles.image} >
+                <img src="Foto centroteste.png" alt=""/>
+            </div>
+            {/* <TEXT
+                characters="“Preservar o meio ambiente é também uma forma
+                            de preservar nossa própria vida”"
+            /> */}
         </div>
     )
 }
--- front-end/src/app/home/Footer/index.tsx
+++ front-end/src/app/home/Footer/index.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import styles from '@/app/home/Footer/styles.module.css'
 
 export const Footer = () => {
